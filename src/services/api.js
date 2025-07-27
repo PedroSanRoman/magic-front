f3c667364ba0c7bbf3e415c970e6ca96af0cb8cf
Pedro San Roman <pedroceferino.sanroman@gmail.com>
2025-07-28 00:16:54 +0200

    front

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL;
-const API = axios.create({ baseURL: `${API_URL}` });
+const API = axios.create({ baseURL: `${API_URL}/api` });
 
 export const searchCards = (query) => API.get(`/cards/search?q=${query}`);
 export const addFavorite = (card) => API.post('/favorites', card);
